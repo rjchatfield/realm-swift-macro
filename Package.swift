7f9f7cd0948af608cb8ebfdcb668b9b2823b5107
Robert J Chatfield <rchatfield@atlassian.com>
2024-02-17 15:36:56 +1000

    tidy moar

--- Package.swift
+++ Package.swift
@@ -6,7 +6,13 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "RealmMacro",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13),
+    ],
     products: [
         .library(
             name: "RealmMacro",
@@ -15,20 +21,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.15.2"),
-        .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.2"),
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.1.2"),
-//        .package(path: "../realm-swift"), // local 'realm-swift'
-        .package(url: "https://github.com/rjchatfield/realm-swift.git", branch: "rob/macros-realm-changes"),
     ],
     targets: [
         // Public API imported by projects
         .target(
             name: "RealmMacro",
-            dependencies: [
-                "RealmMacroMacros",
-                .product(name: "RealmSwift", package: "realm-swift"),
-            ]
+            dependencies: ["RealmMacroMacros"]
         ),
         // Implementation of compiler plugin
         .macro(
@@ -39,20 +37,6 @@ let package = Package(
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-            ]
-        ),
-        // Tests
-        .testTarget(
-            name: "RealmMacroTests",
-            dependencies: [
-                "RealmMacro",
-                "RealmMacroMacros",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
-                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
-                .product(name: "InlineSnapshotTesting", package: "swift-snapshot-testing"),
-                .product(name: "MacroTesting", package: "swift-macro-testing"),
-                .product(name: "CustomDump", package: "swift-custom-dump"),
-                .product(name: "RealmSwift", package: "realm-swift"),
             ]
         ),
     ]
